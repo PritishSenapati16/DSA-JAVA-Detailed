f26b4537abbcdaa83a0cf3a96cceda98235c6060
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-31 20:14:01 +0530

    Add files via upload

---
+++ Ps8Q5.java
@@ -0,0 +1,21 @@
+// Signing in 
+class TommyVecetti{
+    public void hit(){
+        System.out.println("Hitting the enemy");
+    }
+    public void run(){
+        System.out.println("Running away from  the enemy");
+    }
+    public void fire(){
+        System.out.println("Firing on the enemy");
+    }
+}
+public class Ps8Q5 {
+    public static void main(String[] args) {
+        TommyVecetti P1 = new TommyVecetti();
+        P1.hit();
+        P1.run();
+        P1.fire();
+    }
+}
+// Signing off
